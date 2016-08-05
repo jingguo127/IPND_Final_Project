1f2081c7a7f926b0484d91dddc6f3141aae39710
jingguo127 <jingguo127@gmail.com>
2016-08-05 22:06:42 +0800

    Add files via upload

---
+++ tournament.sql
@@ -0,0 +1,22 @@
+-- Table definitions for the tournament project.
+--
+-- Put your SQL 'create table' statements in this file; also 'create view'
+-- statements if you choose to use it.
+--
+-- You can write comments in this file by starting them with two dashes, like
+-- these lines here.
+
+
+CREATE TABLE users(
+id serial primary key, name text);
+
+CREATE TABLE games(
+id integer references users(id), 
+game integer,
+win integer,
+draw integer,
+matches integer,
+opscore integer,
+score integer,
+primary key(id,game));
+
